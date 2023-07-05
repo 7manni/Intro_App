aa7582b02f8af15d581f4c243ffe3433e9815549
Abdulrahman Atta <abood.taef@gmail.com>
2023-07-06 00:13:36 +0400

    Initial Commit

---
+++ 6.1/__1App.swift
@@ -0,0 +1,17 @@
+//
+//  __1App.swift
+//  6.1
+//
+//  Created by Abdulrahman Atta on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct __1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
